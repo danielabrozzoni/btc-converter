7d27f5058d5a8d0e0fa1b11f4a14c3d7a74d9e79
Daniela Brozzoni <danielabrozzoni@protonmail.com>
2021-11-07 16:50:30 +0100

    Use big.js, hide value if selected in the opposite... ...unit picker, small fixes

--- index.js
+++ index.js
@@ -1,33 +1,57 @@
-function update_value(e) {
+// my_index and to_be_updated_index refer to the value that remains static
+// and the one that will be updated.
+function update_value(e, my_index, to_be_updated_index) {
     // Content of our input form
-    let n = document.getElementById("amount1").value;
+    let n = document.getElementById("amount" + my_index).value;
+    if (n == "") {
+        return;
+    }
+    n = new Big(n);
     // Label of the input form to be updated
-    let to_be_updated = "amount2";
-    let my_unit_value = document.getElementById("unit1").value;
-    let opposite_unit_value = document.getElementById("unit2").value;
+    let to_be_updated = "amount" + to_be_updated_index;
+    let my_unit_value = document.getElementById("unit" + my_index).value;
+    let opposite_unit_value = document.getElementById("unit" + to_be_updated_index).value;
 
-    document.getElementById(to_be_updated).value = convert(n, my_unit_value, opposite_unit_value);
+    document.getElementById(to_be_updated).value = convert(n, my_unit_value, opposite_unit_value).toFixed();
+}
+
+function update_unit(e) {
+    // The left value remains the same when changing units, only the
+    // right one is changed.
+    update_value(e, 1, 2);
+    let other_unit;
+    if (e.id == "unit1") {
+        other_unit = "unit2";
+    } else {
+        other_unit = "unit1";
+    }
+    let options = document.getElementById(other_unit).options;
+    for (i in options) {
+        // Hiding the selected option in the other unit picker,
+        // displaying all the others.
+        options[i].hidden = options[i].value == e. value;
+    }
 }
 
 // Convert n from unit1 to unit2
 function convert(n, unit1, unit2) {
-    sats_value = convert_to_sats(n, unit1);
-    return convert_from_sats(sats_value, unit2);
+    btc_value = convert_to_btc(n, unit1);
+    return convert_from_btc(btc_value, unit2);
 }
 
-function convert_to_sats(n, amount) {
-    return n * sats_conversion[amount];
+function convert_to_btc(n, amount) {
+    return n.div(btc_conversion[amount]);
 }
 
-function convert_from_sats(n, amount) {
-    return n / sats_conversion[amount];
+function convert_from_btc(n, amount) {
+    return n.times(btc_conversion[amount]);
 }
 
-let sats_conversion = {
-    "BTC": 1e8,
-    "mBTC": 1e5,
-    "bits": 1e2,
-    "sats": 1,
+let btc_conversion = {
+    "BTC": new Big("1"),
+    "mBTC": new Big("1e3"),
+    "bits": new Big("1e5"),
+    "sats": new Big("1e8"),
     "EUR": null,
     "USD": null,
 };
@@ -35,23 +59,15 @@ let sats_conversion = {
 let eur_price = async () => {
     const response = await fetch('https://api.coinbase.com/v2/prices/spot?currency=EUR');
     const res = await response.json(); //extract JSON from the http response
-    let eur_btc = res["data"]["amount"];
-    // From eur_btc to sats_eur:
-    // eur_sats = eur_btc / sats_conversion["BTC"]
-    // sats_eur = 1/eur_sats
-    // Hence:
-    sats_conversion["EUR"] = sats_conversion["BTC"] / eur_btc;
+    let eur_btc = new Big(res["data"]["amount"].toString());
+    btc_conversion["EUR"] = eur_btc;
 };
 
 let usd_price = async () => {
     const response = await fetch('https://api.coinbase.com/v2/prices/spot?currency=USD');
     const res = await response.json(); //extract JSON from the http response
-    let usd_btc = res["data"]["amount"];
-    // From usd_btc to sats_usd:
-    // usd_sats = usd_btc / sats_conversion["BTC"]
-    // sats_eur = 1/usd_sats
-    // Hence:
-    sats_conversion["USD"] = sats_conversion["BTC"] / usd_btc;
+    let usd_btc = new Big(res["data"]["amount"].toString());
+    btc_conversion["USD"] = usd_btc;
 };
 
 eur_price();
